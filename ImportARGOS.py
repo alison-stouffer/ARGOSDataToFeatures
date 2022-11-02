29acee21a42f6b635c20890fe0486282d04dc873
Alison Stouffer <112555783+alison-stouffer@users.noreply.github.com>
2022-11-02 11:47:10 -0400

    Create empty feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -12,53 +12,60 @@
 
 #%% Import Packages
 
-#Import modules
+#Import modules:
 import sys, os, arcpy
 
-#%% Import Single File as File Object
+#%% Import Single File as File Object & Create Empty Feature Class
 
-# Set input variables (hard-wired)
+# Set input variables (hard-wired):
 inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
 
+# Allow output to be overwritten:
+arcpy.env.overwriteOutput = True
+
+# Create an empty feature class:
+outPath, outName = os.path.split(outputFC)
+arcpy.management.CreateFeatureclass(outPath, outName)
+
 #%% Iterate Through All Lines in the Datafile
 
-# Open the ARGOS data file for reading
+# Open the ARGOS data file for reading:
 inputFileObj = open(inputFile,'r')
 
-# Get the first line of data, so we can use a while loop
+# Get the first line of data:
 lineString = inputFileObj.readline()
 
-# Start the while loop
+# Start the while loop:
 while lineString:
     
     # Set code to run only if the line contains the string "Date: "
     if ("Date :" in lineString):
         
-        # Parse the line into a list
+        # Parse the line into a list:
         lineData = lineString.split()
         
-        # Extract attributes from the datum header line
+        # Extract attributes from the datum header line:
         tagID         = lineData[0]
         date          = lineData[3]
         time          = lineData[4]
         locationClass = lineData[7]
         
-        # Extract location info from the next line
+        # Extract location info from the next line:
         line2String = inputFileObj.readline()
         
-        # Parse the line into a list
+        # Parse the line into a list:
         line2Data = line2String.split()
         
-        # Extract the date we need to variables
+        # Extract the date we need to variables:
         obsLat = line2Data[2]
         obsLon= line2Data[5]
         
-        # Print results to see how we're doing
+        # Print results to see how we're doing:
         print (tagID,"Lat:"+obsLat,"Long:"+obsLon, "Date: "+date, "Time: "+time, "LC:"+locationClass)
         
-    # Move to the next line so the while loop progresses
+    # Move to the next line so the while loop progresses:
     lineString = inputFileObj.readline()
     
-#Close the file object
+#Close the file object:
 inputFileObj.close()
